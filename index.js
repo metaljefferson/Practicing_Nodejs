dc5de5872dee16b49c1f45cde6836c5f2d3a3870
Jefferson Sousa de Paiva <x362624@rede.sp>
2025-02-24 15:48:02 -0300

    shore: practicing Nodejs

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const consign = require("consign");
+const bodyParser = require("body-parser");
+
+
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+consign().include("routes").include('utils').into(app);
+
+app.listen(3000, "127.0.0.1", () => {
+  console.log("Server is running on port 3000");
+});
